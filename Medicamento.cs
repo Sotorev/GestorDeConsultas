14ec29047868c466f8dc5bec09cb108e8079fb2c
Manuel Soto <sotoj7@outlook.com>
2022-06-04 21:21:24 -0600

    Clases necesarias para Json

---
+++ Medicamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_final
+{
+    public class Medicamento
+    {
+        public string Id { get; set; }
+        public string IngredienteGenerico { get; set; }
+        public string Laboratorio { get; set; }
+        public string Enfermedades { get; set; }
+        public int VecesRecetado { get; set; }
+    }
+}
---
+++ Sintoma.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_final
+{
+    public class Sintoma
+    {
+        public string Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
