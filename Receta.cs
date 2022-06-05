4cb724937d32fa26c33c6997974c28aa91f02cfd
Manuel Soto <sotoj7@outlook.com>
2022-06-04 21:24:15 -0600

    Clases derivadas para tener mejor control de la receta y multimedia

---
+++ Receta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_final
+{
+    public class Receta
+    {
+        public List<Medicamento> Medicamentos { get; set; }
+        public string Dosis { get; set; }
+        public string Horario { get; set; }
+    }
+}
