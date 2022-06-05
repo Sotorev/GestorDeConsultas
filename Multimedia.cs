4f0ee8a1e744b31a8088bc28fd32a355b7923dcb
Manuel Soto <sotoj7@outlook.com>
2022-06-04 21:24:15 -0600

    Clases derivadas para tener mejor control de la receta y multimedia

---
+++ Multimedia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_final
+{
+    public class Multimedia
+    {
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+    }
+}
