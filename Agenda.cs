0d4c57db1b5e2974cb5bf275a6f3faeb7a6dd2d9
Manuel Soto <sotoj7@outlook.com>
2022-06-04 21:21:24 -0600

    Clases necesarias para Json

---
+++ Agenda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_final
+{
+    public class Agenda
+    {
+        public string NIT { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime HoraInicio { get; set; }
+        public DateTime HoraFin { get; set; }
+    }
+}
