e23c37cc5c844d8b3b3f622f870b6d076810ebb2
Manuel Soto <sotoj7@outlook.com>
2022-06-04 21:21:24 -0600

    Clases necesarias para Json

---
+++ Historial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_final
+{
+    public class Historial
+    {
+        public int Id { get; set; }
+        public string NIT { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Temperatura { get; set; }
+        public double Presion { get; set; }
+        public List<Sintoma> Sintomas { get; set; }
+        public string Diagnostico { get; set; }
+        public string Tratamiento { get; set; }
+        public Receta Receta { get; set; }
+        public DateTime ProximaCita { get; set; }
+        public int CostoDeConsulta { get; set; }
+        public List<Multimedia> Multimedia { get; set; }
+        //
+        // Constructor:
+        //     Provee un id diferente a cada objeto que se crea a la hora de hacer una consulta.
+        
+    }
+}
---
+++ Paciente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_final
+{
+    public class Paciente
+    {
+        public string NIT { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public DateTime FechaDeNacimiento { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
